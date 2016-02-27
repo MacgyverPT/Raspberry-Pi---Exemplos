1d27ee5aeeb9689533a9510c2c267cfc7955e8f5
MacgyverPT <miguel.nuno.rosa@gmail.com>
2016-02-27 10:10:17 +0000

    Exemplo LED

---
+++ Leds/led.py
@@ -0,0 +1,10 @@
+#!/user/bin/python
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+GPIO.setwarnings(False)
+
+GPIO.setup(17, GPIO.OUT)
+print "Lights ON"
+GPIO.output(17, GPIO.HIGH)
